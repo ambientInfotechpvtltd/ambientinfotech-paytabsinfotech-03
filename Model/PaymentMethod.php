1fc792e69729c399e262a3c14bf044fbeb5aba23
surendraambient01 <61695587+surendraambient01@users.noreply.github.com>
2020-03-11 15:29:25 +0530

    Update PaymentMethod.php

--- Model/PaymentMethod.php
+++ Model/PaymentMethod.php
@@ -2,7 +2,8 @@
 
 /**
  *
- * @copyright  Ambientinfotech
+ * @copyright  Paytabs
+ * @customizedBy  Ambient Infotech
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
