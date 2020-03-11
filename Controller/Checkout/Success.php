d5264f0ee19f3e61b76452390e838519a9fb259e
surendraambient01 <61695587+surendraambient01@users.noreply.github.com>
2020-03-11 15:28:01 +0530

    Update Success.php

--- Controller/Checkout/Success.php
+++ Controller/Checkout/Success.php
@@ -1,7 +1,8 @@
 <?php
 /**
  *
- * @copyright  Ambientinfotech
+ * @copyright  Paytabs
+ * @customizedBy  Ambient Infotech
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
